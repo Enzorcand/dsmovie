97c6fd60ebd9a04637ff624702d6e1f131f9b1e2
Enzo Candiani <enzorcand@gmail.com>
2022-01-17 00:48:44 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,6 +1,11 @@
 import MovieStars from "components/MovieStars";
 
-function MovieScore() {
+type Props = {
+    score: number;
+    count: number;
+}
+
+function MovieScore({ score, count } : Props) {
 
     const score = 3.5;
     const count = 13;
@@ -8,7 +13,7 @@ function MovieScore() {
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score}/>
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
